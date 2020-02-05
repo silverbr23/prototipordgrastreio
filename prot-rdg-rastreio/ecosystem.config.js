22275b440516bea6909cbafda51de295d915c275
silverbr23 <silverbr23@gmail.com>
2020-02-04 21:13:24 -0300

    gogo

--- prot-rdg-rastreio/ecosystem.config.js
+++ prot-rdg-rastreio/ecosystem.config.js
@@ -9,26 +9,15 @@ module.exports = {
     args: 'one two',
     instances: 'max',
     autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
+    watch: true,
+    max_memory_restart: '512mb',
     env: {
       NODE_ENV: 'development'
     },
     env_production: {
       NODE_ENV: 'production'
     }
-  }],
-
-  deploy : {
-    production : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
-    }
-  }
+  }]
 };
 
 
