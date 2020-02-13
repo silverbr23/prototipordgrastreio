9bcfaeb71f4bf51bd512ebef2f595d42df42af04
Rafael Caitano <silverbr23@gmail.com>
2020-02-13 17:17:38 -0300

    gogo

--- prot-rdg-rastreio/routes/index.js
+++ prot-rdg-rastreio/routes/index.js
@@ -1,18 +1,40 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const fetch = require('node-fetch');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/', function(req, res, next) {
+router.post('/', function (req, res, next) {
 
-  if(!req.body.codProduto)
-  res.redirect(`rastreios?codUsuario=${req.body.codUsuario}`);
+  if (!req.body.codProduto)
+    res.redirect(`rastreios?codUsuario=${req.body.codUsuario}`);
 
   res.redirect(`/rastreios/rastreio?codProduto=${req.body.codProduto}`)
 
+});
+
+router.get('/teste', function (req, res, next) {
+  let url = 'http://sapgrcdev.campo.local:51000/RESTAdapter/proc';
+  let auth = Buffer.from('basis:mega@123').toString('base64');
+  console.log(auth);
+  let headers = {
+    "Authorization": `Basic ${auth}`,
+    "Content-Type": "text/plain"
+  }
+  let data = {
+   "idUser": "123",
+   "Pedido": "321"
+  }
+  
+  fetch(url,{method: 'POST', headers: headers, body:data})
+    .then(response => response.json())
+    .then(data => {
+      res.json(data)
+    })
+    .catch(err => res.json(err))
 });
 
 module.exports = router;
--- prot-rdg-rastreio/routes/rastreios.js
+++ prot-rdg-rastreio/routes/rastreios.js
@@ -1,25 +1,72 @@
 var express = require('express');
 var router = express.Router();
 const fs = require('fs');
+const fetch = require('node-fetch');
 let rawdata = fs.readFileSync('./public/backup.json');
 let data = JSON.parse(rawdata);
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
- let dataJ = data.data.filter((a) => a._idUser == req.query.codUsuario).map((a) => a.Pacotes);  
-  dataJ = dataJ[0];
-  if(!dataJ)
-  res.render('not_found');
-  res.render('listarastreio',{dataJ: JSON.stringify(dataJ)});
+router.get('/', function (req, res, next) {
+
+  let url = 'http://sapgrcdev.campo.local:51000/RESTAdapter/proc';
+  let auth = Buffer.from('basis:mega@123').toString('base64');
+  console.log(auth);
+  let headers = {
+    "Authorization": `Basic ${auth}`,
+    "Content-Type": "text/plain"
+  }
+  let data = {
+    "idUser": "123",
+    "Pedido": "321"
+  }
+
+  fetch(url, { method: 'POST', headers: headers, body: data })
+    .then(response => response.json())
+    .then(data => {
+
+      let dataJ = data.MT_Resp.Data.filter((a) => a.idUser == req.query.codUsuario).map((a) => a.Pacotes);
+
+      dataJ = dataJ[0];
+      if (!dataJ)
+        res.render('not_found');
+      res.render('listarastreio', { dataJ: JSON.stringify(dataJ) });
+
+
+    })
+    .catch(err => res.json(err))
+
+
+
 });
 
-router.get('/rastreio', function(req, res, next) {
-  let dataJ = data.data.map(a => a.Pacotes).reduce((acc,cur) =>   {return acc + cur.map((a) =>a)},[])
+router.get('/rastreio', function (req, res, next) {
+    let url = 'http://sapgrcdev.campo.local:51000/RESTAdapter/proc';
+    let auth = Buffer.from('basis:mega@123').toString('base64');
+    console.log(auth);
+    let headers = {
+      "Authorization": `Basic ${auth}`,
+      "Content-Type": "text/plain"
+    }
+    let data = {
+     "idUser": "123",
+     "Pedido": "321"
+    }
+    
+    fetch(url,{method: 'POST', headers: headers, body:data})
+      .then(response => response.json())
+      .then(data => {
 
-  console.log(dataJ);
-  if(!dataJ)
-  res.render('not_found');
-  res.render('rastreio/rastreio');
+        let dataJ = data.MT_Resp.Data.map(a => a.Pacotes).reduce((acc, cur) =>[...acc, ...cur].filter((a)=>a.id == req.query.codProduto), [])
+        dataJ = dataJ[0];
+        
+        if (!dataJ)
+          res.render('not_found');    
+        res.render('rastreio/rastreio', {data:dataJ});
+      
+
+      })
+      .catch(err => res.json(err))
+ 
 });
 
 
